832e8a8d6d0edd97b89da1dae0866d5660d8d240
MarcRGM <marcrodniemanalo@gmail.com>
2026-06-21 10:24:52 +0800

    feat: add numpy vector explorer

---
+++ numpy-vector-explorer/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+data = {
+    "age": [25, 30, 35, None, 40, 120, 29, 31],
+    "income": [50000, 60000, 75000, 80000, None, 95000, 62000, 58000],
+    "score": [85, 90, 78, 92, 88, None, 76, 95]
+}
+
+age = np.array((data["age"]))
+income = np.array(data["income"])
+score = np.array(data["score"])
+
