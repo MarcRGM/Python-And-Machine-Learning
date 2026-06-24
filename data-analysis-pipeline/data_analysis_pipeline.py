c916e38126afd1a394ba146d66b11b5634efac28
MarcRGM <marcrodniemanalo@gmail.com>
2026-06-24 23:47:04 +0800

    feat: add load csv function

--- data-analysis-pipeline/data_analysis_pipeline.py
+++ data-analysis-pipeline/data_analysis_pipeline.py
@@ -2,3 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+def load_data(csv):
+    file_name = csv + ".csv"
+    return pd.read_csv(file_name)
+
+print(load_data("sample_data"))
