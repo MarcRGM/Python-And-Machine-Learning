43ad47029c5da3d51bdda28e67210c65fe6c4c48
MarcRGM <marcrodniemanalo@gmail.com>
2026-06-29 10:22:01 +0800

    chore: add imports

---
+++ linear-regression-explorer/linear_regression_explorer.py
@@ -0,0 +1,4 @@
+import numpy as np # For generating data
+from sklearn.linear_model import LinearRegression # For creating and training the model
+from sklearn.metrics import mean_absolute_error # For making predictions and measuring error
+from sklearn.model_selection import train_test_split # For training
