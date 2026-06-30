0baac1ac22c34f2036a0477405d825d30f7eb12d
MarcRGM <marcrodniemanalo@gmail.com>
2026-06-30 23:55:03 +0800

    feat: add model

--- linear-regression-explorer/linear_regression_explorer.py
+++ linear-regression-explorer/linear_regression_explorer.py
@@ -2,3 +2,6 @@ import numpy as np # For generating data
 from sklearn.linear_model import LinearRegression # For creating and training the model
 from sklearn.metrics import mean_absolute_error # For making predictions and measuring error
 from sklearn.model_selection import train_test_split # For training
+
+model = LinearRegression()
+
