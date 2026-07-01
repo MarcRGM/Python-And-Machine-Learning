f92c791d90cdb76bb24b5a51d8e2f7345d4187f5
MarcRGM <marcrodniemanalo@gmail.com>
2026-07-01 23:34:24 +0800

    docs: comment for np function

--- linear-regression-explorer/linear_regression_explorer.py
+++ linear-regression-explorer/linear_regression_explorer.py
@@ -6,5 +6,5 @@ from sklearn.model_selection import train_test_split # For training
 model = LinearRegression()
 
 house_sizes = np.random.uniform(30.0, 200.0, 50) # low (inclusive), high (exclusive), count
-noise = np.random.normal(0, 20000, 50)
+noise = np.random.normal(0, 20000, 50) # mean, std, count
 price = house_sizes * 1500 + 20000 + noise
